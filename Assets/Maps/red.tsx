d291f92ec6f1c2134f3e28a7091883bbca42f5ae
WorldPierce <bildo316@gmail.com>
2017-10-04 10:26:45 -0400

    map testing

---
+++ Assets/Maps/red.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="red tile" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="../../../../../Downloads/GamesPlusJames - Unity 2D RPG/red-collision-box.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="-1" y="2" width="17" height="13"/>
+  </objectgroup>
+ </tile>
+</tileset>
